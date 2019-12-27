a4a65977f2e880f55cbd01ae483ed4750291d262
caoshanqiang <leo422@foxmail.com>
2019-12-27 15:59:39 +0800

    python

---
+++ qwe.py
@@ -0,0 +1,27 @@
+# coding:utf8
+ 
+#'''创建服务器端程序，用来接收客户端传进的数据'''
+
+from socket import *
+from time import ctime
+ 
+def server():
+    HOST = ''
+    PORT = 10521
+    ADDR = (HOST,PORT)
+    server_socket = socket(AF_INET,SOCK_STREAM)
+    server_socket.bind(ADDR)
+    server_socket.listen(5)
+    while True:
+        print 'Waiting for connecting ......'
+        tcpclientsocket,addr = server_socket.accept()
+        print 'Connected by ',addr
+        while True:
+            data = tcpclientsocket.recv(1024)
+            if not data:
+                break
+            print data
+        tcpclientsocket.close()
+    server_socket.close()
+ 
+server()
